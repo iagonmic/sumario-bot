dd4ffa7fb69026c8a21435d663d388f90b6b638e
iagonmic <iagoflavio_sa@hotmail.com>
2025-12-03 11:48:42 -0300

    parte de gerador de analise finalizada

--- src/ai/analysis_generator.py
+++ src/ai/analysis_generator.py
@@ -2,12 +2,58 @@
 Geração de análises para cada dimensão.
 """
 
-from utils.logger import get_logger
+from agno.agent import Agent
+from agno.models.groq import Groq
+from src.utils.logger import get_logger
+from dotenv import load_dotenv
+from yaml import safe_load
+from json import load, dump
+
+with open("src/config.yaml", "r") as f:
+    config = safe_load(f)
+
+data_path = f"{config['paths']['examples_dir']}/exemplo_musica.json"
+
+with open(data_path, 'r') as d:
+    data = load(d)
+
+load_dotenv()
 
 logger = get_logger("analysis")
 
-def generate_analysis(dimensao: dict) -> str:
-    logger.info(f"Gerando análise para {dimensao['nome']}")
+def generate_analysis(dimensao_nome, secao) -> str:
 
-    # TODO: integrar com Agno / IA real
-    return f"Análise gerada automaticamente para a dimensão {dimensao['nome']}."
+    agent = Agent(model=Groq(id='openai/gpt-oss-20b'),
+    description="Você é um analista de dados especialista no âmbito acadêmico",
+    instructions=[
+        "Sempre responda em português",
+        "Se atenha exclusivamente aos dados e ao que eles querem dizer",
+        "Analise especificamente de acordo com o dado, não seja generalista",
+        "Considere todo o contexto acadêmico e o modelo de balanced scorecard ao produzir a análise",
+        "Não forneça recomendações"
+        ],
+    expected_output="Retorne uma resposta enxuta, de 2 a 4 linhas"
+)
+    
+    response = agent.run(
+        f"""Faça uma análise para a dimensao {dimensao_nome}, seção {secao}, seguindo uma estrutura parecida com: O índice de ocupação dos egressos é moderado,
+        indicando que pouco mais da metade dos formados está inserida no mercado de trabalho. O resultado aponta a necessidade de ampliar
+        o vínculo entre a formação acadêmica e as oportunidades profissionais, especialmente no contexto local e regional.""")
+
+    return response.content
+
+def save_to_json(data):
+    for i, dimensao in enumerate(data['dimensoes']):
+        logger.info(f"Gerando análise para dimensão {dimensao}")
+
+        for j, secao in enumerate(data['dimensoes'][i]['secoes']):
+            logger.info(f"Gerando análise para seção {secao}")
+            response = generate_analysis(dimensao['nome'], secao)
+            data['dimensoes'][i]['secoes'][j]['analise'] = response
+    
+    data_save_path = f"{config['paths']['examples_dir']}/exemplo_musica_analise.json"
+
+    with open(data_save_path, 'w', encoding='utf-8') as file:
+        dump(data, file, indent=4, ensure_ascii=False)
+
+save_to_json(data)
--- src/ai/recommendations_generator.py
+++ src/ai/recommendations_generator.py
@@ -2,12 +2,53 @@
 Gera recomendações para cada dimensão.
 """
 
-from utils.logger import get_logger
+from agno.agent import Agent
+from agno.models.groq import Groq
+from src.utils.logger import get_logger
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logger = get_logger("recommendations")
 
-def generate_recommendations(dimensao: dict, analise: str) -> str:
+def generate_recommendations(dimensao: dict) -> str:
     logger.info(f"Gerando recomendações para {dimensao['nome']}")
 
-    # TODO: integrar com IA
-    return f"Recomendações baseadas na análise da dimensão {dimensao['nome']}."
+    agent = Agent(model=Groq(id='openai/gpt-oss-20b'),
+    description="Você é um analista de dados especialista no âmbito acadêmico",
+    instructions=[
+        "Sempre responda em português",
+        "Sua tarefa é fornecer recomendações baseada na análise fornecida e aos dados, para que a reitoria que leia essas recomendações consiga tomar alguma atitude, seja de curto prazo ou longo prazo"
+        "Se atenha a análise fornecida, juntamente aos dados e ao que eles querem dizer, não invente informações",
+        "Analise especificamente de acordo com a análise, não seja generalista",
+        "Considere todo o contexto acadêmico e o modelo de balanced scorecard ao produzir as recomendações"
+        ],
+    expected_output="Retorne em formato de bullet points, de 3 a 5 bullet points, sendo eles separados por -"
+)
+    
+    response = agent.run(
+        f"""Forneça recomendações sobre a dimensão {dimensao}, seguindo uma estrutura parecida com: 
+        - Fortalecer parcerias com empresas, órgãos públicos e organizações sociais para a inserção de egressos.
+        - Incentivar projetos de extensão e estágios que aproximem o estudante do mercado de trabalho.
+        - Criar mecanismos de acompanhamento de egressos para identificar áreas de baixa empregabilidade e ajustar a formação profissional.""")
+    
+    return response.content"
+
+# teste
+
+from yaml import safe_load
+from json import load
+
+with open("src/config.yaml", "r") as f:
+    config = safe_load(f)
+
+data_path = f"{config['paths']['examples_dir']}/exemplo_musica.json"
+
+with open(data_path, 'r') as d:
+    data = load(d)
+
+for i, dimensao in enumerate(data['dimensoes']):
+    response = generate_recommendations(dimensao)
+    print(response)
+    if i == 0:
+        break
